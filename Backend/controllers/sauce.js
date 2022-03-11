81d2c1dbae530b25ebec21ff18f4fc7d7060f57e
Pomares Jonathan <pomares.jonathan@gmail.com>
2022-03-11 15:44:04 +0100

    gestion du paramètre de modification

--- Backend/controllers/sauce.js
+++ Backend/controllers/sauce.js
@@ -18,7 +18,7 @@ exports.getOneSauce = (req, res, next) => {
     .catch((error) => res.status(404).json({ error }));
 }
 
-//Middleware de cration des sauces dans la bdd 
+//Middleware de creation des sauces dans la bdd 
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
@@ -35,29 +35,23 @@ exports.createSauce = (req, res, next) => {
 exports.modifySauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
-      if (sauce.userId !== req.auth.userId) {
+      if (sauce.userId !== req.auth.userId) { //si la requête est présenter par le même id que celui qui l'a poster => error
         res.status(403).json({ error: "Requête non autorisée" });
-      } else if (sauce.userId == req.auth.userId) {
-        const filename = sauce.imageUrl.split("/images/")[1];
-        fs.unlink(`images/${filename}`, () => {
-          const sauceObject = req.file
-            ? {
-                ...JSON.parse(req.body.sauce),
-                imageUrl: `${req.protocol}://${req.get("host")}/images/${
-                  req.file.filename
-                }`,
-              }
-            : { ...req.body };
-          Sauce.updateOne(
-            { _id: req.params.id },
-            { ...sauceObject, _id: req.params.id }
-          )
-            .then(() => res.status(200).json({ message: "Sauce modifiée." }))
-            .catch((error) => res.status(400).json({ error }));
-        });
       }
-    })
-    .catch((error) => res.status(400).json({ error }));
+      if (req.file) { //si une image existe => supression de l'image
+        const filename = sauce.imageUrl.split("/images/")[1];
+        fs.unlink(`images/${filename}`, () => {});
+      }
+      const sauceObject = req.file ? //vérifie si une image existe et applique les paramètres de modification
+          {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+          } : { ...req.body };
+      Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Sauce modifiée." }))
+        .catch((error) => res.status(400).json({ error }));
+  })
+  .catch((error) => res.status(400).json({ error }));
 };
 
 //Middleware de suppression d'une sauce spécifique
@@ -79,50 +73,44 @@ exports.deleteSauce = (req, res, next) => {
 };
 
 //Middleware de gestion des likes ou dislikes
+//$inc = incorpore les éléments rentrer en paramètre
+//$push = ajoute l'id sur le like ou dislike
+//$pull = supprime l'id sur le like ou le dislike
 exports.likeOrDislike = (req, res, next) => {
   if (req.body.like === 1) {
-    Sauce.updateOne(
-      { _id: req.params.id },
+    Sauce.updateOne({ _id: req.params.id },
       {
         $inc: { likes: req.body.like++ },
         $push: { usersLiked: req.body.userId },
-      }
-    )
+      })
       .then((sauce) => res.status(200).json({ message: "Ajout Like" }))
       .catch((error) => res.status(400).json({ error }));
   } else if (req.body.like === -1) {
-    Sauce.updateOne(
-      { _id: req.params.id },
+    Sauce.updateOne({ _id: req.params.id },
       {
         $inc: { dislikes: req.body.like++ * -1 },
         $push: { usersDisliked: req.body.userId },
-      }
-    )
+      })
       .then((sauce) => res.status(200).json({ message: "Ajout Dislike" }))
       .catch((error) => res.status(400).json({ error }));
   } else {
     Sauce.findOne({ _id: req.params.id })
       .then((sauce) => {
         if (sauce.usersLiked.includes(req.body.userId)) {
-          Sauce.updateOne(
-            { _id: req.params.id },
-            { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } }
-          )
-            .then((sauce) => {
-              res.status(200).json({ message: "Suppression Like" });
+          Sauce.updateOne({ _id: req.params.id },
+            { 
+              $pull: { usersLiked: req.body.userId }, 
+              $inc: { likes: -1 } 
             })
+            .then((sauce) => {res.status(200).json({ message: "Suppression Like" });})
             .catch((error) => res.status(400).json({ error }));
         } else if (sauce.usersDisliked.includes(req.body.userId)) {
-          Sauce.updateOne(
-            { _id: req.params.id },
+          Sauce.updateOne({ _id: req.params.id },
             {
               $pull: { usersDisliked: req.body.userId },
               $inc: { dislikes: -1 },
-            }
-          )
-            .then((sauce) => {
-              res.status(200).json({ message: "Suppression Dislike" });
             })
+            .then((sauce) => {res.status(200).json({ message: "Suppression Dislike" });})
             .catch((error) => res.status(400).json({ error }));
         }
       })
