6625167ed03be228ecad06e90ab0b9004bd42218
Pomares Jonathan <pomares.jonathan@gmail.com>
2022-03-11 15:48:18 +0100

    gestion de la modification sauce

--- Backend/controllers/sauce.js
+++ Backend/controllers/sauce.js
@@ -73,14 +73,11 @@ exports.deleteSauce = (req, res, next) => {
 };
 
 //Middleware de gestion des likes ou dislikes
-//$inc = incorpore les éléments rentrer en paramètre
-//$push = ajoute l'id sur le like ou dislike
-//$pull = supprime l'id sur le like ou le dislike
 exports.likeOrDislike = (req, res, next) => {
   if (req.body.like === 1) {
     Sauce.updateOne({ _id: req.params.id },
       {
-        $inc: { likes: req.body.like++ },
+        $inc: { likes: req.body.like++ }, //$inc = incorpore les éléments rentrer en paramètre
         $push: { usersLiked: req.body.userId },
       })
       .then((sauce) => res.status(200).json({ message: "Ajout Like" }))
@@ -89,7 +86,7 @@ exports.likeOrDislike = (req, res, next) => {
     Sauce.updateOne({ _id: req.params.id },
       {
         $inc: { dislikes: req.body.like++ * -1 },
-        $push: { usersDisliked: req.body.userId },
+        $push: { usersDisliked: req.body.userId }, //$push = ajoute l'id sur le like ou dislike
       })
       .then((sauce) => res.status(200).json({ message: "Ajout Dislike" }))
       .catch((error) => res.status(400).json({ error }));
@@ -99,7 +96,7 @@ exports.likeOrDislike = (req, res, next) => {
         if (sauce.usersLiked.includes(req.body.userId)) {
           Sauce.updateOne({ _id: req.params.id },
             { 
-              $pull: { usersLiked: req.body.userId }, 
+              $pull: { usersLiked: req.body.userId }, //$pull = supprime l'id sur le like ou le dislike
               $inc: { likes: -1 } 
             })
             .then((sauce) => {res.status(200).json({ message: "Suppression Like" });})
